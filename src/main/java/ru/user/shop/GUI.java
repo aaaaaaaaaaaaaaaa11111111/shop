e90c0338c23d5f38be9a718c483224f2f08edb5c
aaaaaaaaaaaaaaaa11111111 <241308118+aaaaaaaaaaaaaaaa11111111@users.noreply.github.com>
2025-11-15 14:25:05 +0500

    [CODE] Update GUI

--- src/main/java/ru/user/shop/GUI.java
+++ src/main/java/ru/user/shop/GUI.java
@@ -2,47 +2,176 @@ package ru.user.shop;
 
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
-import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Stream;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.table.DefaultTableModel;
 
 public class GUI extends JFrame {
-	public GUI() {
+
+	private Database database;
+
+	public GUI(Database database) {
 		super("Shop");
+
+		this.database = database;
+
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
 
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
 		Dimension screenSize = toolkit.getScreenSize();
-		setLayout(new GridLayout(10, 10));
-		setSize((int) screenSize.getWidth(), (int) screenSize.getHeight());
+		int width = (int) screenSize.getWidth();
+		int height = (int) screenSize.getHeight();
+		width -= width / 5;
+		height -= height / 5;
 
-		GridBagConstraints gbc1 = new GridBagConstraints();
-		gbc1.gridx = 0;
-		gbc1.gridy = 0;
+		setSize(width, height);
+		setLayout(new GridBagLayout());
+
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(5, 5, 5, 5);
+
 		JComboBox<String> b1 = new JComboBox<String>();
+		b1.setPreferredSize(new Dimension(125, 25));
+		gbc.gridx = 0;
+		gbc.gridy = 0;
 		b1.addItem("Вывести");
-		b1.addItem("Искать");
 		b1.addItem("Вставить");
-		add(b1, gbc1);
+		b1.addItem("Обновить");
+		add(b1, gbc);
 
-		GridBagConstraints gbc2 = new GridBagConstraints();
-		gbc2.gridx = 0;
-		gbc2.gridy = 1;
 		JComboBox<String> b2 = new JComboBox<String>();
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+
+		b2.setPreferredSize(new Dimension(125, 25));
 		b2.addItem("Покупатель");
 		b2.addItem("Сотрудник");
 		b2.addItem("Магазин");
-		add(b2, gbc2);
+		add(b2, gbc);
 
-		GridBagConstraints gbc3 = new GridBagConstraints();
-		gbc3.gridx = 0;
-		gbc3.gridy = 1;
-		add(new JButton("Выполнить"), gbc3);
+		JComboBox<String> b3 = new JComboBox<String>();
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		b3.setPreferredSize(new Dimension(125, 25));
+		add(b3, gbc);
 
+		JButton button = new JButton("Выполнить");
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		button.setPreferredSize(new Dimension(125, 25));
+		add(button, gbc);
+
+		JTextArea area = new JTextArea("Поле для поиска");
+
+		gbc.gridx = 0;
+		gbc.gridy = 4;
+		area.setPreferredSize(new Dimension(125, 25));
+		add(area, gbc);
+
+		JTable table = new JTable();
+		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setPreferredSize(new Dimension(width - width / 5, height / 2));
+
+		gbc.gridx = 1;
+		gbc.gridy = 5;
+		add(scrollPane, gbc);
+
+		// add(table, gbc);
+
+		b1.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				onAction(b1, b2, b3);
+			}
+		});
+
+		b2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				onAction(b1, b2, b3);
+			}
+		});
+
+		button.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				switch ((String) b1.getSelectedItem()) {
+				case "Вывести" -> {
+					switch ((String) b2.getSelectedItem()) {
+					case "Покупатель" -> {
+						switch ((String) b3.getSelectedItem()) {
+						case "Всех" -> {
+							setCustomerInTable(table, database.getAllCustomers());
+						}
+						case "По ФИО" -> {
+							setCustomerInTable(table, database.searchCustomerByFullName(area.getText()));
+						}
+						}
+					}
+					}
+
+				}
+				}
+			}
+		});
+
+		onAction(b1, b2, b3);// нужно для первичного запуска
 		setVisible(true);
 	}
+
+	private void onAction(JComboBox<String> b1, JComboBox<String> b2, JComboBox<String> b3) {
+		b3.removeAllItems();
+
+		switch ((String) b1.getSelectedItem()) {
+		case "Вывести" -> {
+			switch ((String) b2.getSelectedItem()) {
+			case "Покупатель" -> {
+				b3.addItem("Всех");
+				b3.addItem("По ФИО");
+			}
+			}
+		}
+		}
+	}
+
+	private void setCustomerInTable(JTable table, List<Customer> customers) {
+		Class<?> clazz = Customer.class;
+		Field[] fields = clazz.getDeclaredFields();
+
+		Object[][] data = new Object[customers.size()][fields.length];
+		for (int i = 0; i < data.length; i++) {
+			Customer customer = customers.get(i);
+			for (int j = 0; j < fields.length; j++) {
+				Field field = customer.getClass().getDeclaredFields()[j];
+				try {
+					field.setAccessible(true);
+					data[i][j] = field.get(customer);
+					field.setAccessible(false);
+					DefaultTableModel model = new DefaultTableModel(data,
+							Stream.of(fields).map(x -> x.getName()).toArray());
+					table.setModel(model);
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
 }
--- src/main/java/ru/user/shop/Main.java
+++ src/main/java/ru/user/shop/Main.java
@@ -1,18 +1,8 @@
 package ru.user.shop;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.sql.Date;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.Scanner;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
-import org.apache.commons.cli.help.HelpFormatter;
+import javax.swing.SwingUtilities;
 
 public class Main {
 
@@ -20,54 +10,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		// этот кусок кода разрывает соединение с бд в случае отключения программы
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> closeDatabase()));
-
-		Options options = new Options();
-
-		options.addRequiredOption("h", "host", true, "Hostname");
-		options.addRequiredOption("p", "port", true, "Port");
-		options.addRequiredOption("d", "database", true, "Database");
-		options.addRequiredOption("u", "username", true, "Username");
-		options.addRequiredOption("ps", "password", true, "Password");
-
-		// TODO дописать описание
-		options.addOption("l", "list", true, "Lists tables, customers, employees, shops, warehouses, suppliers, ...");
-		options.addOption("i", "insert", true, "Insert customer, employee, shop, warehouse, ...");
-
-		CommandLineParser parser = new DefaultParser();
-		HelpFormatter formatter = HelpFormatter.builder().get();
-		CommandLine cmd;
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			closeDatabase();
+			System.out.println("Конец работы программы");
+		}));
 
 		try {
-			cmd = parser.parse(options, args);
-		} catch (ParseException e) {
-			System.err.println(e.getMessage());
-			try {
-				formatter.printHelp("shop", "", options, "", false);
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-			System.exit(1);
-			return;
-		}
+			String host = "192.168.56.101";
+			int port = 5432;
+			String databaseName = "shop";
+			String username = "admin";
+			String password = "123456";
 
-		String host = cmd.getOptionValue("host");
-		int port;
-		try {
-			port = Integer.parseInt(cmd.getOptionValue("port"));
-		} catch (NumberFormatException e) {
-			System.out.println("Неправильно введен порт");
-			System.exit(1);
-			return;
-		}
-		String databaseName = cmd.getOptionValue("database");
-		String username = cmd.getOptionValue("username");
-		String password = cmd.getOptionValue("password");
-
-		try {
 			database = new Database(host, port, databaseName, username, password);
 			System.out.println("Успешное соединение с БД");
-			new GUI();
+
+			SwingUtilities.invokeLater(() -> {
+				new GUI(database);
+			});
 		} catch (SQLException e) {
 			System.out.println("Не удалось подключиться к БД, проверьте введенные данные на правильность");
 			e.printStackTrace();
@@ -79,217 +39,7 @@ public class Main {
 			System.exit(1);
 			return;
 		}
-
-		// database.searchCustomerByFullName("о").forEach((x) ->
-		// System.out.println(x.toString()));;
-		// database.searchEmployeeByFullName("о").forEach((x) ->
-		// System.out.println(x.toString()));;
-//		database.searchProductByName("о").forEach((x) -> System.out.println(x.toString()));;
-//		database.searchProductByName("г").forEach((x) -> System.out.println(x.toString()));;
-//		database.searchProductBySupplyId(1).forEach((x) -> System.out.println(x.toString()));;
-//		database.searchEmployeeByShopId(1).forEach((x) -> System.out.println(x.toString()));;
-//		database.searchSupplierByName("оооо").forEach((x) -> System.out.println(x.toString()));
-//		database.searchCheckByCustomerId(1).forEach((x) -> System.out.println(x.toString()));
-		// database.searchSupplyBySupplierId(1).forEach((x) ->
-		// System.out.println(x.toString()));
-		database.searchPositionByCheckId(1).forEach((x) -> System.out.println(x.toString()));
-
-		executeCommand(cmd);
-		System.out.println("Конец работы программы");
-	}
-
-	public static void executeCommand(CommandLine cmd) {
-		if (cmd.hasOption("list")) {
-			// break не нужен, тк case "" -> {} сам ставит его, при case "": { } нужен
-			switch (cmd.getOptionValue("list")) {
-			case "tables" -> {
-				List<String> tables = database.getTables();
-				if (tables.isEmpty()) {
-					System.out.println("Таблиц нет");
-				} else {
-					System.out.println("Таблицы:");
-					tables.forEach((table) -> {
-						System.out.println(table);
-					});
-				}
-			}
-			case "customers" -> {
-				List<Customer> customers = database.getAllCustomers();
-				if (customers.isEmpty()) {
-					System.out.println("Покупателей нет");
-				} else {
-					System.out.println("Покупатели:");
-					customers.forEach((customer) -> {
-						System.out.println(customer.toString());
-					});
-				}
-			}
-			case "employees" -> {
-				List<Employee> employees = database.getAllEmployees();
-				if (employees.isEmpty()) {
-					System.out.println("Сотрудников нет");
-				} else {
-					System.out.println("Сотрудники:");
-					employees.forEach((employee) -> {
-						System.out.println(employee.toString());
-					});
-				}
-			}
-			case "shops" -> {
-				List<Shop> shops = database.getAllShops();
-				if (shops.isEmpty()) {
-					System.out.println("Магазинов нет");
-				} else {
-					System.out.println("Магазины:");
-					shops.forEach((shop) -> {
-						System.out.println(shop.toString());
-					});
-				}
-			}
-			case "warehouses" -> {
-				List<Warehouse> warehouses = database.getAllWarehouses();
-				if (warehouses.isEmpty()) {
-					System.out.println("Складов нет");
-				} else {
-					System.out.println("Склады:");
-					warehouses.forEach((warehouse) -> {
-						System.out.println(warehouse.toString());
-					});
-				}
-			}
-			case "suppliers" -> {
-				List<Supplier> suppliers = database.getAllSuppliers();
-				if (suppliers.isEmpty()) {
-					System.out.println("Поставщиков нет");
-				} else {
-					System.out.println("Поставщики:");
-					suppliers.forEach((warehouse) -> {
-						System.out.println(warehouse.toString());
-					});
-				}
-			}
-			case "checks" -> {
-				List<Check> checks = database.getAllChecks();
-				if (checks.isEmpty()) {
-					System.out.println("Чеков нет");
-				} else {
-					System.out.println("Чеки:");
-					checks.forEach((check) -> {
-						System.out.println(check.toString());
-					});
-				}
-			}
-			case "supplies" -> {
-				List<Supply> supplies = database.getAllSupplies();
-				if (supplies.isEmpty()) {
-					System.out.println("Поставок нет");
-				} else {
-					System.out.println("Поставки:");
-					supplies.forEach((supply) -> {
-						System.out.println(supply.toString());
-					});
-				}
-			}
-			case "products" -> {
-				List<Product> products = database.getAllProducts();
-				if (products.isEmpty()) {
-					System.out.println("Продуктов нет");
-				} else {
-					System.out.println("Продукты:");
-					products.forEach((supply) -> {
-						System.out.println(supply.toString());
-					});
-				}
-			}
-			default -> {
-				System.out.println("Такого параметра нет");
-			}
-			}
-			return;
-		}
-
-		if (cmd.hasOption("insert")) {
-			Scanner scanner = new Scanner(System.in, Charset.forName("Windows-1251"));
-			switch (cmd.getOptionValue("insert")) {
-			case "customer" -> {
-				insertCustomer(scanner);
-			}
-			case "employee" -> {
-				insertEmployee(scanner);
-			}
-			case "shop" -> {
-				insertShop(scanner);
-			}
-			case "warehouse" -> {
-				insertWarehouse(scanner);
-			}
-			default -> {
-				System.out.println("Такого параметра нет");
-			}
-			}
-			scanner.close();
-			return;
-		}
-	}
-
-	public static void insertCustomer(Scanner scanner) {
-		System.out.println("Введите ФИО");
-		String fullName = scanner.nextLine();
-		System.out.println("Введите почту");
-		String email = scanner.nextLine();
-		System.out.println("Введите номер телефона");
-		long phoneNumber = getPhoneNumberFromInput(scanner);
-		if (database.insertCustomer(new Customer(fullName, email, phoneNumber))) {
-			System.out.println("Покупатель добавлен в базу");
-		} else {
-			System.out.println("Покупатель не добавлен в базу");
-		}
-	}
-
-	public static void insertEmployee(Scanner scanner) {
-		System.out.println("Введите ФИО");
-		String fullName = scanner.nextLine();
-		System.out.println("Введите почту");
-		String email = scanner.nextLine();
-		System.out.println("Введите номер телефона");
-		long phoneNumber = getPhoneNumberFromInput(scanner);
-		System.out.println("Введите дату рождения в формате (YYYY-MM-DD)");
-		Date birthdayDate = getDateFromInput(scanner);
-		System.out.println("Введите дату трудоустройства в формате (YYYY-MM-DD)");
-		Date dateOfEmployment = getDateFromInput(scanner);
-		System.out.println("Введите статус");
-		String status = scanner.nextLine();
-		System.out.println("Введите ID магазина, где работает сотрудник");
-		int shopId = getIntFromInput(scanner);
-//TODO тут должно запрашивать фото, пока пропустим
-		if (database.insertEmployee(
-				new Employee(fullName, email, phoneNumber, birthdayDate, dateOfEmployment, status, shopId, "PHOTO"))) {
-			System.out.println("Сотрудник добавлен в базу");
-		} else {
-			System.out.println("Сотрудник не добавлен в базу");
-		}
-	}
-
-	public static void insertShop(Scanner scanner) {
-		System.out.println("Введите адрес");
-		String address = scanner.nextLine();
-		if (database.insertShop(new Shop(address))) {
-			System.out.println("Магазин добавлен в базу");
-		} else {
-			System.out.println("Магазин не добавлен в базу");
-		}
-	}
-
-	public static void insertWarehouse(Scanner scanner) {
-		System.out.println("Введите адрес");
-		String address = scanner.nextLine();
-		System.out.println("Введите ID магазина, к которому принадлежит склад");
-		int shopId = getIntFromInput(scanner);
-		if (database.insertWarehouse(new Warehouse(address, shopId))) {
-			System.out.println("Склад добавлен в базу");
-		} else {
-			System.out.println("Склад не добавлен в базу");
-		}
+		System.out.println("Конец main");
 	}
 
 	public static void closeDatabase() {
@@ -301,48 +51,6 @@ public class Main {
 				e.printStackTrace();
 			}
 		}
-	}
-
-	private static int getIntFromInput(Scanner scanner) {
-		int a;
-		while (true) {
-			try {
-				a = Integer.parseInt(scanner.nextLine());
-				break;
-			} catch (NumberFormatException e) {
-				System.out.println("Неправильно введено число, повторите попытку");
-			}
-		}
-		return a;
-	}
-
-	private static long getPhoneNumberFromInput(Scanner scanner) {
-		long phoneNumber;
-		while (true) {
-			try {
-				phoneNumber = Long.parseLong(scanner.nextLine());
-				break;
-			} catch (NumberFormatException e) {
-				System.out.println("Неправильно введен номер, повторите попытку");
-			}
-		}
-		return phoneNumber;
-	}
-
-	private static Date getDateFromInput(Scanner scanner) {
-		Date date;
-		while (true) {
-			try {
-				String input = scanner.nextLine();
-				String[] split = input.split("-");
-				date = new Date(Integer.parseInt(split[0]) - 1900, Integer.parseInt(split[1]) + 1,
-						Integer.parseInt(split[2]));
-				break;
-			} catch (Exception e) {
-				System.out.println("Неправильно введена дата, повторите попытку");
-			}
-		}
-		return date;
 	}
 
 }
